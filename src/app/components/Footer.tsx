53c106ca866d71d3fb9470bb6153cdae2c673956
narendra <51484021+alenway@users.noreply.github.com>
2023-08-19 22:24:46 +0530

    footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import "./style.css"
+
+const Footer = () => {
+    return (
+        <div className="bg">
+            <div className="container mx-auto py-20 px-10 text-white">
+                <div className="grid grid-cols-4">
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-lg font-medium">Solutions</h1>
+                        <ul className="text-lg font-medium ">
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Hoots</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Kuwago</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Data wise</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Pique</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-lg font-medium">Solutions</h1>
+                        <ul className="text-lg font-medium ">
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Hoots</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Kuwago</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Data wise</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Pique</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-lg font-medium">Solutions</h1>
+                        <ul className="text-lg font-medium ">
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Hoots</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Kuwago</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Data wise</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Pique</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-lg font-medium">Solutions</h1>
+                        <ul className="text-lg font-medium ">
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Hoots</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Kuwago</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Data wise</a>
+                            </li>
+                            <li>
+                                <a className='opacity-50 hover:opacity-100' href="">Pique</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import TopSection from './components/TopSection'
 import CombinatioSection from './components/CombinatioSection'
 import { EnterpriseSection } from './components/EnterpriseSection'
 import DetailSection from './components/DetailSection'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <CombinatioSection />
       <EnterpriseSection />
       <DetailSection />
+      <Footer />
     </div>
   )
 }
